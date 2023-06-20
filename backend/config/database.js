82b074efec3f63c67ce99175d42e928f05daf8cd
Om <omchate48@gmail.com>
2023-06-20 23:10:11 +0530

    created product models

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,17 @@
-const mongoose =require("mongoose");
-mongoose.connect("mongo://localhost")
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+        useNewUrlParser: true, useUnifiedTopology: true, family: 4,
+
+    })
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    }).catch((err)=>{
+        console.log(err)
+
+    })
+};
+
+module.exports = connectDatabase;
